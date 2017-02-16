49ef6d78b88a551ee4afd958d831f681924b9e62
yelnats321 <elnar.dakeshov@gmail.com>
2017-02-15 21:51:11 -0800

    Add boost lisence
    We legal now

--- EntityPlus/test/test_main.cpp
+++ EntityPlus/test/test_main.cpp
@@ -1,2 +1,7 @@
+//          Copyright Elnar Dakeshov 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #define CATCH_CONFIG_MAIN 
 #include <catch.hpp>
