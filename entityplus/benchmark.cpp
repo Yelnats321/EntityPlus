59ff247d3be83685cf63089570498176af39405b
yelnats321 <elnar.dakeshov@gmail.com>
2017-02-20 18:50:11 -0800

    Add benchmark code

---
+++ entityplus/benchmark.cpp
@@ -0,0 +1,90 @@
+#if !NDEBUG
+static_assert(false, "Running in debug move");
+#endif
+
+#include <entityplus/entity.h>
+#include <entityx/entityx.h>
+#include <chrono>
+#include <iostream>
+
+class Timer {
+	std::chrono::high_resolution_clock::time_point start;
+	const char *str;
+public:
+	Timer(const char *str): start(std::chrono::high_resolution_clock::now()),
+		str(str) {}
+	~Timer() {
+		auto end = std::chrono::high_resolution_clock::now();
+		std::cout << str << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "\n";
+	}
+};
+
+void entPlusTest(int entityCount, int iterationCount, int tagProb) {
+	using namespace entityplus;
+	entity_manager<component_list<int>, tag_list<struct Tag>> em;
+	std::cout << "EntityPlus\n";
+	{
+		Timer timer("Add entities: ");
+		for (int i = 0; i < entityCount; ++i) {
+			auto ent = em.create_entity();
+			ent.add_component<int>(i);
+			if (i % tagProb == 0)
+				ent.set_tag<Tag>(true);
+		}
+	}
+	{
+		Timer timer("For_each entities: ");
+		std::uint64_t sum = 0;
+		for (int i = 0; i < iterationCount; ++i) {
+			em.for_each<Tag, int>([&](auto ent, auto i) {
+				sum += i;
+			});
+		}
+		std::cout << sum << "\n";
+	}
+}
+
+void entXTest(int entityCount, int iterationCount, int tagProb) {
+	using namespace entityx;
+	struct Tag {};
+	entityx::EntityX ex;
+	std::cout << "EntityX\n";
+	{
+		Timer timer("Add entities: ");
+		for (int i = 0; i < entityCount; ++i) {
+			auto ent = ex.entities.create();
+			ent.assign<int>(i);
+			if (i % tagProb == 0)
+				ent.assign<Tag>();
+		}
+	}
+	{
+		Timer timer("For_each entities: ");
+		std::uint64_t sum = 0;
+		for (int i = 0; i < iterationCount; ++i) {
+			ex.entities.each<Tag, int>([&](auto ent, auto &, auto i) {
+				sum += i;
+			});
+		}
+		std::cout << sum << "\n";
+	}
+}
+
+void runTest(int entityCount, int iterationCount, int tagProb) {
+	std::cout << "Count: " << entityCount
+		<< " ItrCount: " << iterationCount
+		<< " TagProb: " << tagProb << "\n";
+	entPlusTest(entityCount, iterationCount, tagProb);
+	std::cout << "\n";
+	entXTest(entityCount, iterationCount, tagProb);
+	std::cout << "\n\n";
+}
+
+int main() {
+	runTest(1'000, 1'000'000, 3);
+	runTest(10'000, 1'000'000, 3);
+	runTest(30'000, 100'000, 3);
+	runTest(100'000, 100'000, 5);
+	runTest(10'000, 1'000'000, 1'000);
+	runTest(100'000, 1'000'000, 1'000);
+}
