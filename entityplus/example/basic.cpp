a21ab8d3bdde4034b409f5c4a8029c8b373a020c
yelnats321 <elnar.dakeshov@gmail.com>
2017-04-07 17:33:34 -0700

    Fix example, better cmake

--- entityplus/example/basic.cpp
+++ entityplus/example/basic.cpp
@@ -4,14 +4,17 @@
 using namespace entityplus;
 
 int main() {
-	entity_manager<component_list<int>, tag_list<>> entityManager;
+	using entity_manager_t = entity_manager<component_list<int>, tag_list<>>;
+	using entity_t = entity_manager_t::entity_t;
+
+	entity_manager_t entityManager;
 	for (int i = 0; i < 50; ++i) {
 		entityManager.create_entity(i);
 	}
 
 	auto sum = 0;
 	entityManager.for_each<int>(
-			[&](auto ent, int i) {
+			[&](entity_t ent, int i) {
 			sum +=i;
 			});
 	std::cout << sum << "\n";
--- entityplus/example/events.cpp
+++ entityplus/example/events.cpp
@@ -16,17 +16,19 @@ struct CombatEvent {
 int main() {
 	using components = component_list<int, Name>;
 	using tags = tag_list<struct Player>;
-	using entity_t = typename entity_manager<components, tags>::entity_t;
+	using entity_t = entity_manager<components, tags>::entity_t;
 
 	entity_manager<components, tags> entityManager;
 	event_manager<components, tags, CombatEvent<entity_t>> eventManager;
 
 	entityManager.set_event_manager(eventManager);
 
-	eventManager.subscribe<entity_created<entity_t>>([](const auto &) {
+	eventManager.subscribe<entity_created<entity_t>>(
+			[](const entity_created<entity_t> &) {
 		std::cout << "entity_created\n";
 	});	
-	eventManager.subscribe<entity_destroyed<entity_t>>([](const auto &ev) {
+	eventManager.subscribe<entity_destroyed<entity_t>>(
+			[](const entity_destroyed<entity_t> &ev) {
 		std::cout << "entity_destroyed with:\n";
 		if (ev.entity.has_component<int>())
 			std::cout << "int: " << ev.entity.get_component<int>() << "\n";
@@ -35,19 +37,18 @@ int main() {
 		if (ev.entity.has_tag<Player>())
 			std::cout << "Player tag\n";
 	});
-	eventManager.subscribe<component_added<entity_t, int>>([](const auto &ev) {
+	eventManager.subscribe<component_added<entity_t, int>>(
+			[](const component_added<entity_t, int> &ev) {
 		std::cout << "int component added with value: " << ev.component << "\n";
 	});
 
 	for (int i = 0; i < 10; ++i) {
 		entityManager.create_entity(i);
 	}
-	auto player = entityManager.create_entity();
+	entity_t player = entityManager.create_entity();
 	player.set_tag<Player>(true);
 	player.add_component(Name{"Player"});
 
-	auto ents = entityManager.get_entities<>();
-	for (auto &ent : ents) ent.destroy();
-	char c;
-	std::cin >> c;
+	for (entity_t &ent : entityManager.get_entities<>())
+		ent.destroy();
 }
